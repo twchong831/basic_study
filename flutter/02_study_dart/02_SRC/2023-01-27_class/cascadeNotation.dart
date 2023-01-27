a50f5d9b30492353a48ec24b9f9cb34a90514dbb
twchong831 <choungtw@gmail.com>
2023-01-27 17:58:13 +0900

    add dart files

---
+++ flutter/02_study_dart/02_SRC/2023-01-27_class/cascadeNotation.dart
@@ -0,0 +1,34 @@
+// cascade notation
+class Player {
+  String name;
+  int xp;
+  String team;
+
+  Player({required this.name, required this.xp, required this.team});
+
+  void sayHello() {
+    print("Hi[json] my name is $name");
+    print("\txp : $xp");
+    print("\team : $team");
+  }
+}
+
+void main(List<String> args) {
+  var tw = Player(name: 'tw', xp: 1200, team: 'red');
+  tw.name = 'tw-1';
+  tw.xp = 10;
+  tw.team = 'blue';
+  tw.sayHello();
+
+  var tw2 = Player(name: 'tw2', xp: 100, team: 'blue')
+    ..name = "tw2-2"
+    ..xp = 10000
+    ..team = 'red'
+    ..sayHello();
+
+  var potato = tw2
+    ..name = 'potato'
+    ..xp = 2003
+    ..team = 'black'
+    ..sayHello();
+}
--- flutter/02_study_dart/02_SRC/2023-01-27_class/constructors.dart
+++ flutter/02_study_dart/02_SRC/2023-01-27_class/constructors.dart
@@ -23,6 +23,81 @@ class Player2 {
   }
 }
 
+//constructors named parameters
+class Player3 {
+  final String name;
+  int xp;
+  String team;
+  int age;
+
+  Player3({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  void sayHello() {
+    print("Hi my name is $name");
+    print("\txp : $xp");
+    print("\team : $team");
+    print("\tage : $age");
+  }
+}
+
+//named constructors.
+class Player4_color {
+  final String name;
+  int xp, age;
+  String team;
+
+  Player4_color({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
+
+  Player4_color.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player4_color.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
+  void sayHello() {
+    print("Hi my name is $name");
+    print("\txp : $xp");
+    print("\team : $team");
+    print("\tage : $age");
+  }
+}
+
+//json
+class Player5_json {
+  final String name;
+  int xp;
+  String team;
+
+  Player5_json.fromJson(Map<String, dynamic> playerJson)
+      : name = playerJson['name'],
+        xp = playerJson['xp'],
+        team = playerJson['team'];
+
+  void sayHello() {
+    print("Hi[json] my name is $name");
+    print("\txp : $xp");
+    print("\team : $team");
+  }
+}
+
 void main(List<String> args) {
   var player = Player("name,tw", 1500);
   // print(player.name);
@@ -32,4 +107,50 @@ void main(List<String> args) {
 
   var player3 = Player2("name:twtw", 2000);
   player3.sayHello();
+
+  //----------------------
+  // var player4 = Player3("twtww2", 1000, "red", 30);
+  var player4 = Player3(
+    name: "twtww2",
+    xp: 1000,
+    team: "red",
+    age: 30,
+  );
+
+  player4.sayHello();
+
+  //---------------------
+  var blu_player = Player4_color.createBluePlayer(
+    name: "tw(blue)",
+    age: 30,
+  );
+
+  var red_player = Player4_color.createRedPlayer("tw(red)", 30);
+
+  blu_player.sayHello();
+  red_player.sayHello();
+
+  //----------------------
+  var apiData = [
+    {
+      "name": "tw",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "tw2",
+      "team": "red",
+      "xp": 0,
+    },
+    {
+      "name": "tw3",
+      "team": "red",
+      "xp": 0,
+    },
+  ];
+
+  apiData.forEach((playerJson) {
+    var playerJ = Player5_json.fromJson(playerJson);
+    playerJ.sayHello();
+  });
 }
