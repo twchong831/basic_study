f533bdf64c473b8202627d5a405ffc601b23394c
twchong831 <choungtw@gmail.com>
2023-02-08 18:18:42 +0900

    study flutter & make widget button..wq

--- flutter/03_flutter/README.md
+++ flutter/03_flutter/README.md
@@ -9,3 +9,35 @@
 cd [dir]
 flutter create [project_name]
 ```
+
+## setting
+
+### user settings in vscode
+
+- 자동으로 const 항목에 const를 추가
+
+```json
+// add
+"editor.codeActionsOnSave": {
+  "source.fixAll": true
+ },
+//-------
+ "dart.openDevTools": "flutter",
+ "latex-workshop.view.pdf.viewer": "tab",
+ "[latex]": {
+  "editor.defaultFormatter": "James-Yu.latex-workshop"
+ },
+ "[tex]": {
+ },
+```
+
+- 소스코드 가이드라인 생성
+
+```json
+"dart.previewFlutterUiGuides": true,
+```
+
+- code Action with VSCode
+- 단축키 : [ cmd + . ]
+
+![ex codeaction](./image/img_flutter_codeAction.png)
--- flutter/03_flutter/SRC/2023-02-07_firstAPP/newfirstapp/lib/main.dart
+++ flutter/03_flutter/SRC/2023-02-07_firstAPP/newfirstapp/lib/main.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:newfirstapp/widgets/buttons.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818), // set background color
+        backgroundColor: const Color(0xFF181818), // set background color
         body: Padding(
           // set padding
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             // make column
             children: [
-              const SizedBox(
+              SizedBox(
                 //임의의 위젯을 삽입
                 height: 80,
-                // child: Container(  // sizedwidget 확인용 색 할당
-                //   color: Colors.blue,
-                // ),
+                child: Container(
+                  // sizedwidget 확인용 색 할당
+                  color: Colors.blue,
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end, //정렬
@@ -48,6 +55,54 @@ class App extends StatelessWidget {
                       ),
                     ],
                   )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+                child: Container(
+                  color: Colors.blue, // check sizedbox
+                ),
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 44,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(
+                    text: 'Transfer',
+                    bgColor: Colors.amber,
+                    textcolor: Colors.black,
+                  ),
+                  Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textcolor: Colors.white,
+                  ),
                 ],
               )
             ],
