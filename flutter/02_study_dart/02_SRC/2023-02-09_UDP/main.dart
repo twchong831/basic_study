54cea7a88ce2b4950443b341f434ff26ff44891f
twchong831 <choungtw@gmail.com>
2023-02-09 16:59:39 +0900

    add dart udp ex

---
+++ flutter/02_study_dart/02_SRC/2023-02-09_UDP/main.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //ip: 192.168.4.60
+  //port : 5004
+  late List total;
+
+  final int outputInverval = 50;
+
+  // RawDatagramSocket.bind(InternetAddress.anyIPv4, 5004)
+  RawDatagramSocket.bind(InternetAddress('192.168.4.60'), 5004)
+      .then((RawDatagramSocket socket) {
+    print('Datagram socket ready to receive');
+    print('${socket.address.address}:${socket.port}');
+
+    socket.listen((RawSocketEvent e) {
+      Datagram? d = socket.receive();
+      if (d == null) return;
+
+      //check size & sum datagram
+      if (d.data.length == 32752) {
+        total = d.data;
+      } else if (d.data.length == 26456) {
+        if (total.length == 32752) {
+          total += d.data;
+        }
+      }
+
+      // String messgae = new String.fromCharCodes(d.data).trim();
+      // print('Datagram from ${d.address.address}:${d.port}: ${messgae}');
+      //16진수로 출력
+      // var str_hex = d.data.map((e) => e.toRadixString(16));
+      // print('Input [${d.data.length}]: $str_hex');
+
+      if (total.length == 32752 + 26456) {
+        // var str_hex = total.map((e) => e.toRadixString(16));
+        // print('Input [${total.length}]: $str_hex');
+        print('Input [${total.length}] : ');
+        for (var i = 0; i < total.length; i += outputInverval) {
+          late var str;
+          if (i + outputInverval > total.length)
+            str =
+                total.getRange(i, total.length).map((e) => e.toRadixString(16));
+          else
+            str = total
+                .getRange(i, i + outputInverval)
+                .map((e) => e.toRadixString(16));
+          print(str);
+        }
+      }
+    });
+  });
+}
