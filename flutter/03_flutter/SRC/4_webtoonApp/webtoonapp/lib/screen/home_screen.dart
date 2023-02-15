24981439dfb8753cd352777433e7eda10f528773
twchong831 <choungtw@gmail.com>
2023-02-15 17:52:10 +0900

    feat: update webtoon app

--- flutter/03_flutter/SRC/4_webtoonApp/webtoonapp/lib/screen/home_screen.dart
+++ flutter/03_flutter/SRC/4_webtoonApp/webtoonapp/lib/screen/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:webtoonapp/models/webtoon_model.dart';
+import 'package:webtoonapp/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+/*
+  //using statefulwidget
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+  
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
 
   @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+*/
+  @override
   Widget build(BuildContext context) {
+    // print(webtoons);
+    // print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -18,6 +41,91 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          // snapshot : ui 변화를 알려줌
+          if (snapshot.hasData) {
+            /*return ListView(
+              children: [
+                for (var webtoon in snapshot.data!) Text(webtoon.title)
+              ],
+            );*/
+            /*
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                // print(index); // index는 스크롤 될때에만 증감함.
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+            );
+            */
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }, //draw ui
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        // index는 스크롤 될때에만 증감함.
+        var webtoon = snapshot.data![index];
+        // return Text(webtoon.title);
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge, // image round
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15), // image round
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 21,
+                // fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
--- flutter/03_flutter/SRC/4_webtoonApp/webtoonapp/lib/services/api_service.dart
+++ flutter/03_flutter/SRC/4_webtoonApp/webtoonapp/lib/services/api_service.dart
@@ -5,12 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:webtoonapp/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
 // https dev package
 // pubspec.yaml에 패키지 추가
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
 
     final url = Uri.parse('$baseUrl/$today');
