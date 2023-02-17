ce7df26c18ca484e5a2d4e0446683ca9f9844c13
twchong831 <choungtw@gmail.com>
2023-02-17 11:44:39 +0900

    feat: update app

--- flutter/03_flutter/SRC/app_udpNetwork/udpcommu/lib/screen/home_screen.dart
+++ flutter/03_flutter/SRC/app_udpNetwork/udpcommu/lib/screen/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:udpcommu/widgets/checkable_icon_button_widget.dart';
 import 'package:udpcommu/widgets/text_editor_widget.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,16 +13,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String ipAddress = '127.0.0.1';
   int portNum = 5000;
+  bool isCheckedConnect = false;
+
+  // final double minWidth = 500;
+  // final double minHight = 100;
 
   WidgetTextField ipText = WidgetTextField(
     label: 'IP Address',
     hint: 'xxx.xxx.xxx.xxx',
   );
+  WidgetTextField portText = WidgetTextField(
+    label: 'Port Num.',
+    hint: '5000',
+    onlyNum: true,
+  );
 
-  beUpdated() {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void onCheckedConnect() {
     setState(() {
-      ipAddress = ipText.getData();
+      isCheckedConnect = !isCheckedConnect;
+      if (isCheckedConnect) {
+        if (ipText.getData().isNotEmpty) {
+          ipAddress = ipText.getData();
+        }
+        if (portText.getData().isNotEmpty) {
+          portNum = int.parse(portText.getData());
+        }
+      }
     });
+
+    if (isCheckedConnect) {
+      print("IP/port : $ipAddress / $portNum");
+    }
   }
 
   @override
@@ -40,28 +65,23 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Expanded(
                   child: ipText,
-                  // child: WidgetTextField(
-                  //   label: 'IP Address',
-                  //   hint: 'xxx.xxx.xxx.xxx',
-                  // ),
                 ),
                 const SizedBox(
                   width: 10,
                 ),
                 Expanded(
-                  child: WidgetTextField(
-                    label: 'Port Num.',
-                    hint: '5000',
-                  ),
+                  child: portText,
                 ),
               ],
             ),
-            CheckableIconWidget(
-              iconOn: const Icon(Icons.radio_button_on_rounded),
-              iconOff: const Icon(Icons.radio_button_off_rounded),
-              size: 40,
-              ip: ipText.getData(),
+            IconButton(
+              onPressed: onCheckedConnect,
+              icon: isCheckedConnect
+                  ? const Icon(Icons.radio_button_on_rounded)
+                  : const Icon(Icons.radio_button_off_rounded),
+              iconSize: 40,
             ),
+            Text('SET : $ipAddress $portNum'),
           ],
         ),
       ),
--- flutter/03_flutter/SRC/app_udpNetwork/udpcommu/lib/widgets/text_editor_widget.dart
+++ flutter/03_flutter/SRC/app_udpNetwork/udpcommu/lib/widgets/text_editor_widget.dart
@@ -1,37 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class WidgetTextField extends StatelessWidget {
+class WidgetTextField extends StatefulWidget {
   final String label;
   final String hint;
+  final bool onlyNum;
+
+  // String inputTxt = "";
 
   WidgetTextField({
     super.key,
     required this.label,
     required this.hint,
+    this.onlyNum = false,
   });
 
   final edController = TextEditingController();
 
+  @override
+  State<WidgetTextField> createState() => _WidgetTextFieldState();
+
   String getData() {
-    print("call get data");
     return edController.text;
+  }
+}
+
+class _WidgetTextFieldState extends State<WidgetTextField> {
+  // final edController = TextEditingController();
+
+  void isCHANGED() {
+    // if (edController.text.isNotEmpty) {
+    //   // print("changed txt");
+    //   widget.inputTxt = edController.text;
+    // }
+  }
+
+  // void isChanged(String txt) {
+  //   print("controller : ${edController.text}");
+  //   print("input : $txt");
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    // edController.addListener((isCHANGED));
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: edController,
-      decoration: InputDecoration(
-        label: Text(label),
-        labelStyle: const TextStyle(
-          fontSize: 20,
+    if (!widget.onlyNum) {
+      // text mode
+      return TextField(
+        controller: widget.edController,
+        decoration: InputDecoration(
+          label: Text(widget.label),
+          labelStyle: const TextStyle(
+            fontSize: 20,
+          ),
+          hintText: widget.hint,
+          hintStyle: TextStyle(
+            color: Colors.black.withOpacity(0.5),
+          ),
+          border: const OutlineInputBorder(),
         ),
-        hintText: hint,
-        hintStyle: TextStyle(
-          color: Colors.black.withOpacity(0.5),
+        // onChanged: (value) => isChanged(value),
+      );
+    } else {
+      // only number mode
+      return TextField(
+        controller: widget.edController,
+        keyboardType: TextInputType.number,
+        inputFormatters: [
+          FilteringTextInputFormatter.digitsOnly, // limit number input only
+        ],
+        decoration: InputDecoration(
+          label: Text(widget.label),
+          labelStyle: const TextStyle(
+            fontSize: 20,
+          ),
+          hintText: widget.hint,
+          hintStyle: TextStyle(
+            color: Colors.black.withOpacity(0.5),
+          ),
+          border: const OutlineInputBorder(),
         ),
-        border: const OutlineInputBorder(),
-      ),
-    );
+        // onChanged: (value) => isChanged(value),
+      );
+    }
   }
 }
