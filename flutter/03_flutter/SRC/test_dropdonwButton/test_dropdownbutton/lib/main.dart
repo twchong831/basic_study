4a5358be59d6b6796e4ac2ea99066b298f8f02b4
twchong831 <choungtw@gmail.com>
2023-02-22 13:34:32 +0900

    chore: complete dropdownbutton test app

--- flutter/03_flutter/SRC/test_dropdonwButton/test_dropdownbutton/lib/main.dart
+++ flutter/03_flutter/SRC/test_dropdonwButton/test_dropdownbutton/lib/main.dart
@@ -18,13 +18,43 @@ class _MyAppState extends State<MyApp> {
     '3',
     '4',
   ];
-  final List<String> dropDownList2 = [
-    'st',
-    'stt',
-    'stttt',
-  ];
+
+  List<DropdownMenuItem<String>> get dropdownItems1 {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(value: "USA", child: Text("USA")),
+      const DropdownMenuItem(value: "Canada", child: Text("Canada")),
+      const DropdownMenuItem(value: "Brazil", child: Text("Brazil")),
+      const DropdownMenuItem(value: "England", child: Text("England")),
+    ];
+    return menuItems;
+  }
+
+  List<DropdownMenuItem<String>> get dropdownItems2 {
+    List<DropdownMenuItem<String>> menuItems = [
+      const DropdownMenuItem(value: "korea", child: Text("korea")),
+      const DropdownMenuItem(value: "USA", child: Text("USA")),
+      const DropdownMenuItem(value: "Canada", child: Text("Canada")),
+      const DropdownMenuItem(value: "Brazil", child: Text("Brazil")),
+      const DropdownMenuItem(value: "England", child: Text("England")),
+    ];
+    return menuItems;
+  }
+
+  List<DropdownMenuItem<String>> setSecondDropDownMenu(int sel) {
+    List<DropdownMenuItem<String>> nullVal = [
+      const DropdownMenuItem(value: "0.0.0.0", child: Text("0.0.0.0")),
+    ];
+    if (sel == 1) {
+      return dropdownItems1;
+    } else if (sel == 2) {
+      return dropdownItems2;
+    }
+
+    return nullVal;
+  }
 
   String selectItem = '1';
+  String selectItem2 = '';
 
   // This widget is the root of your application.
   @override
@@ -38,25 +68,40 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Drop_down TEST'),
         ),
-        body: Row(children: [
-          DropdownButton(
-            value: selectItem,
-            items: dropDownList.map(
-              (String item) {
-                return DropdownMenuItem(
-                  value: item,
-                  child: Text(item),
-                );
-              },
-            ).toList(),
-            onChanged: (dynamic value) {
-              setState(() {
-                selectItem = value;
-                print("update -> $selectItem");
-              });
-            },
-          )
-        ]),
+        body: Row(
+          children: [
+            Row(
+              children: [
+                DropdownButton(
+                  // value: selectItem,
+                  items: dropDownList.map(
+                    (String item) {
+                      return DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (dynamic value) {
+                    setState(() {
+                      selectItem = value;
+                      print("update -> $selectItem");
+                    });
+                  },
+                ),
+                DropdownButton(
+                  // value: selectItem2,
+                  items: setSecondDropDownMenu(int.parse(selectItem)),
+                  onChanged: (dynamic value) {
+                    setState(() {
+                      selectItem2 = value;
+                    });
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
